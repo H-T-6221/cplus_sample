3546b33c3313739fe259dfb7ebf60e44d28f579e
H-T-6221 <ichikawa.hisato.1226@gmail.com>
2024-10-04 15:29:06 +0900

    Add 20241004 script create container for build and build execute

---
+++ 03_automation/automation_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker exec -it sender server
+docker exec -it reciever client
---
+++ 03_automation/build_container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./create_container.sh build 192.168.20.10
+
+docker exec -it build git clone https://github.com/H-T-6221/cplus_sample.git
+docker exec -it build chmod 755 cplus_sample/04_source/build.sh
+docker exec -it build ./cplus_sample/04_source/build.sh
+
+docker cp build:/root/cplus_sample/04_source/module .
